e8516238892c6dee24cc4e2a1bf05e836e95abfc
Sarah Allen <sarah@ultrasaurus.com>
2019-09-21 17:13:27 -0700

    connect to gitter via IRC

--- README.md
+++ README.md
@@ -1,13 +1,35 @@
 
-## Understanding IRC protocol
+## Getting started with IRC
 
 1. join gitter
 2. get IRC password from: https://irc.gitter.im/
-3. on the command-lin
+3. on the command-line
 
 ```
 socat -v tcp4-listen:1234,reuseaddr,fork ssl:irc.gitter.im:6667,verify=0
 ```
+
+4. in another terminal window, in local repo directory
+
+```cargo run```
+
+socat output:
+```
+> 2019/09/21 17:07:51.380401  length=123 from=0 to=122
+PASS 20dfff9c75f6b2a239f86223fea60c652d20f824\r
+NICK ultrasaurus_twitter\r
+USER ultrasaurus_twitter 0 * ultrasaurus_twitter\r
+< 2019/09/21 17:07:51.476298  length=82 from=0 to=81
+:ultrasaurus_twitter!ultrasaurus_twitter@irc.gitter.im NICK :ultrasaurus_twitter\r
+
+```
+
+
+
+
+## Understanding IRC protocol
+
+The folllowing was captured with an IRC client
 
 ```
 > 2019/09/21 14:12:28.166479  length=123 from=0 to=122
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,27 @@
 use tokio::net::TcpStream;
 use tokio::prelude::*;
+use std::env;
 use std::error::Error;
 
 //https://docs.rs/tokio/0.2.0-alpha.5/tokio/net/tcp/struct.TcpStream.html
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    let irc_user = env::var("USER")?;
+    let irc_pass = env::var("PASS")?;
+
     // Connect to a peer
     let mut stream = TcpStream::connect("127.0.0.1:1234").await?;
 
     // Write some data.
-    stream.write_all(b"USER ").await?;
+    let connect_str = format!("PASS {pass}\r\nNICK {name}\r\nUSER {name} 0 * {name}\r\n", 
+            pass=irc_pass, name=irc_user);
+    stream.write_all(connect_str.as_bytes()).await?;
+
+    // Read some data
+    // TODO
+
+    // let join_str = format!("JOIN #irc-tokio\r\nPRIVMSG #irc-tokio :Hello from my first Rust IRC client!\r\n");
+    // stream.write_all(join_str.as_bytes()).await?;
 
     Ok(())
 }
