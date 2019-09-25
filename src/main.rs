b63a09a92a2073410f6df72fe0d3b80c747497a2
Sarah Allen <sarah@ultrasaurus.com>
2019-09-24 19:11:06 -0700

    compiles with read_line

--- src/main.rs
+++ src/main.rs
@@ -29,20 +29,11 @@ async fn main() -> Result<(), Box<dyn Error>> {
     // :ultrasaurus_twitter!ultrasaurus_twitter@irc.gitter.im NICK :ultrasaurus_twitter\r
 
     let mut response = String::new();
-    stream.read_line(response).await?;
-    println!("{}", response);
-
-    // failed with this error:
-
-    // error[E0599]: no method named `read_line` found for type `tokio_net::tcp::stream::TcpStream` in the current scope
-    //   --> src/main.rs:32:12
-    //    |
-    // 32 |     stream.read_line(response).await?;
-    //    |            ^^^^^^^^^
-    //    |
-    //    = note: the method `read_line` exists but the following trait bounds were not satisfied:
-    //            `tokio_net::tcp::stream::TcpStream : tokio_io::io::async_buf_read_ext::AsyncBufReadExt`
-
+    let mut reader = tokio::io::BufReader::new(stream);
+    reader.read_line(&mut response).await?;
+    println!("response: {}", response);
+    // output
+    // response: :ultrasaurus_twitter!ultrasaurus_twitter@irc.gitter.im NICK :ultrasaurus_twitter
 
 
     // TODO
