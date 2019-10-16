b25b563b98d407885fd71afa549fa3f8bae9e120
Sarah Allen <sarah@ultrasaurus.com>
2019-10-16 00:27:57 -0700

    adjust workflow for bot interaction
    rough draft of API design in comments
    
    Co-authored-by: Jen-Mei Wu <jenmei@oaktownlabs.com>

--- src/main.rs
+++ src/main.rs
@@ -14,37 +14,30 @@ struct Session<'a> {
 }
 
 impl Session<'_> {
-  async fn read_expect<'a>(&'a mut self, expected_response: &'a str) -> Result<(), Box<dyn Error>> {
-    let mut response = String::new();
-    self.stream.read_line(&mut response).await?;
-    if response == expected_response.to_string() {
-      Ok(())
-    } else {
-      let error_str = format!("expected: {}, got: {}", expected_response, response);
-      panic!(error_str)      // not sure what's a good pattern for returning an error here
-    }
-  }
-
-  async fn connect<'a>(addr: &'a str, nick: &'a str, pass: &'a str) -> Result<Session<'a>, Box<dyn Error>> {
+  async fn new<'a>(addr: &'a str, nick: &'a str) -> Result<Session<'a>, Box<dyn Error>> {
     let tcp = TcpStream::connect(addr).await?;
     let stream = BufReader::new(tcp); 
-    let mut session = Session {
+    Ok(Session {
       nick,
       stream
-    };
-    
-    let connect_str = format!("PASS {pass}\r\nNICK {name}\r\nUSER {name} 0 * {name}\r\n", 
-        pass=pass, name=nick);
-    session.stream.write_all(connect_str.as_bytes()).await?;
-
-    session.read_expect(":ultrasaurus_twitter!ultrasaurus_twitter@irc.gitter.im NICK :ultrasaurus_twitter\r\n").await?;
-    session.read_expect(":irc.gitter.im 001 ultrasaurus_twitter Gitter :ultrasaurus_twitter!ultrasaurus_twitter@irc.gitter.im\r\n").await?;
-    session.read_expect(":irc.gitter.im 002 ultrasaurus_twitter Version: 1.4.0\r\n").await?;
-
-    Ok(session)
+    })
   }
 
+  async fn connect<'a>(&'a mut self, pass: &'a str) -> Result<(), Box<dyn Error>> {
+    let connect_str = format!("PASS {pass}\r\nNICK {name}\r\nUSER {name} 0 * {name}\r\n", 
+        pass=pass, name=self.nick);
+    self.stream.write_all(connect_str.as_bytes()).await?; 
+    
+    let mut response = String::new();
+    loop {
+      self.stream.read_line(&mut response).await?;
+      println!("{}", response);
+    }
+
+    Ok(())
+  }
 }
+
 
 //https://docs.rs/tokio/0.2.0-alpha.5/tokio/net/tcp/struct.TcpStream.html
 #[tokio::main]
@@ -56,7 +49,28 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     // Connect to the server
     let addr = "127.0.0.1:1234";
-    let irc = Session::connect(addr, &irc_user, &irc_pass).await?;
+    let mut irc = Session::new(addr, &irc_user).await?;
+
+    // let join_response = format!("{name}@irc.gitter.im JOIN #tokio-rs/tokio\r\n", 
+    //     name=irc_user);
+
+    // irc.match_str("Joining #ultrasaurus", join_response, {
+    //   // do something
+    // });
+
+
+    // irc.match_str("Joining ultrasaurus", join_response, ultra_handler);
+
+    // irc.match_fn("When pinged", "fn", ping_handler);
+    // irc.match_regex("when asked for help", "/^@irc_user: help/", help_handler);
+
+    // irc.event_disconnect("when disconnected", reconnect);
+    // irc.init("/join #ultrasaurus");
+    irc.connect(&irc_pass).await?;    // read loop
+    // irc.command("/join #ultrasaurus");
+ 
+    //tokio::run(whatever);
+
 
 
     Ok(())
